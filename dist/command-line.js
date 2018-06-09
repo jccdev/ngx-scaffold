4e62ea4447601a69db7947de7f5c8deab60078a8
Jordan Castillo <Jordan.Castillo@energytransfer.com>
2018-06-09 11:11:06 -0500

    removed dist folder

--- dist/command-line.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const commandLineArgs = require("command-line-args");
-const optionsDefinitions = [
-    { name: 'help', alias: 'h' },
-];
-const options = commandLineArgs(optionsDefinitions);
-//# sourceMappingURL=command-line.js.map
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const commandLineArgs = require("command-line-args");
-const optionsDefinitions = [
-    { name: 'help', alias: 'v' },
-];
-const options = commandLineArgs(optionsDefinitions);
-const test = '';
-//# sourceMappingURL=index.js.map
